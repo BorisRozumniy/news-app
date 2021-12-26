e36b9a4a3ded508879874628c824f219ae53138e
Boris <rozumniy.boris.work@gmail.com>
2021-12-26 13:34:59 +0200

    get data from api in the App

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
+
+const api =
+  'https://newsapi.org/v2/everything?q=tesla&from=2021-11-26&sortBy=publishedAt&apiKey=792e66b37a6c47cea817424a68757032';
+
+type response = {
+  articles: news[];
+  status: string;
+  totalResults: number;
+};
+
+type news = {
+  author: string;
+  content: string;
+  description: string;
+  publishedAt: string;
+  source: { id: string; name: string };
+  title: string;
+  url: string;
+  urlToImage: string;
+};
 
 function App() {
+  const [newsList, setNewsList] = useState([] as news[]);
+  useEffect(() => {
+    fetch(api)
+      .then((res) => res.json())
+      .then(
+        (result: response) => {
+          console.log(2, result);
+          // setIsLoaded(true);
+          setNewsList(result.articles);
+        },
+        (error) => {
+          console.log(3, error);
+          // setIsLoaded(true);
+          // setError(error);
+        },
+      );
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {newsList.length > 0 &&
+        newsList.map((newsItem) => (
+          <li key={newsItem.publishedAt}>
+            <h2>{newsItem.title}</h2>
+            <p>published {newsItem.publishedAt}</p>
+            <h3>author: {newsItem.author}</h3>
+            <p>description {newsItem.description}</p>
+            <p>{newsItem.content}</p>
+            <img src={newsItem.urlToImage} alt={newsItem.title} />
+            <a href={newsItem.url}>source</a>
+          </li>
+        ))}
     </div>
   );
 }
